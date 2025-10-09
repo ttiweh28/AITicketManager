b1ec2c10915d2aa22b0e0870917c547202ab2bd8
ttiweht <hewitttusiime82@gmail.com>
2025-10-09 17:47:00 -0500

    Initial Commit

---
+++ AITicketManager/src/main/java/com/aiticketmanager/AiTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.aiticketmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTicketManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTicketManagerApplication.class, args);
+        System.out.println("🚀 AI Ticket Manager is running...");
+    }
+}
---
+++ AITicketManager/src/main/java/com/aiticketmanager/model/User.java
@@ -0,0 +1,4 @@
+package com.aiticketmanager.model;
+
+public class User {
+}
