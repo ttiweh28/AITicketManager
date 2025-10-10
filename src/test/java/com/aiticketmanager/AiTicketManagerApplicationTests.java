e98142626fac8a75022fff8dd05a8e1c0ed4d5c0
ttiweht <hewitttusiime82@gmail.com>
2025-10-10 12:13:22 -0500

    Working on CI

--- src/test/java/com/aiticketmanager/AiTicketManagerApplicationTests.java
+++ src/test/java/com/aiticketmanager/AiTicketManagerApplicationTests.java
@@ -2,12 +2,14 @@ package com.aiticketmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AiTicketManagerApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
