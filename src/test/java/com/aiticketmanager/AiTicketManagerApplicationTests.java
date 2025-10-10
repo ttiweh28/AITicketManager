037b6f57a778fda8945220dd72cde35ccedfa998
ttiweht <hewitttusiime82@gmail.com>
2025-10-10 12:35:50 -0500

    still working on CI

--- src/test/java/com/aiticketmanager/AiTicketManagerApplicationTests.java
+++ src/test/java/com/aiticketmanager/AiTicketManagerApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 class AiTicketManagerApplicationTests {
 
